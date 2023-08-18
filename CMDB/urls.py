a39827f70def0d110ad6be8cad32d29d6b5b26bb
jfling13 <jfling13@gmail.com>
2023-08-18 11:10:17 -0400

    增加入口路由

--- CMDB/urls.py
+++ CMDB/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('', include('assets.urls')),
     path('admin/', admin.site.urls),
     path('assets/', include('assets.urls')),
 ]
