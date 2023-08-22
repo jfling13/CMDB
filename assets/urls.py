fccb6ec1e4f26e3b4e75d28a406c5c50932c24c6
jfling13 <jfling13@gmail.com>
2023-08-22 13:53:10 -0400

    add login urls

--- assets/urls.py
+++ assets/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 from assets import views
+from django.contrib.auth.views import LoginView
 
 app_name = 'assets'
 
 urlpatterns = [
     path('', views.dashboard),
+    path('login/',views.login, name="login"),
     path('index/', views.index, name="index"),
     path('dashboard/', views.dashboard,name="dashboard"),
     path('detail/<int:asset_id>/', views.detail, name="detail"),
     path('report/', views.report, name='report'),
+    path('logout/',views.logout,name='logout'),
+    
 ]
